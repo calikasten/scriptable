d904c4fd4ecf65dc28ff9583e1f223e678cad2e6
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2023-05-03 11:49:45 -0500

    Add files via upload

---
+++ Static Text Widget.js
@@ -0,0 +1,26 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: font;
+let widget = await createWidget()
+
+// Check where the script is running
+if (config.runsInWidget) {
+  Script.setWidget(widget)
+} 
+else {
+  widget.presentSmall()
+}
+Script.complete();
+
+// Display widget
+async function createWidget() {
+	let testWidget = new ListWidget()
+  // Add widget background
+		testWidget.backgroundColor = new Color("#000000");
+	// Add text to a widget
+	let widgetText = testWidget.addText("INSERT TEXT HERE");
+		widgetText.font = Font.boldSystemFont(15)
+		widgetText.textColor = new Color ("#ffffff")
+		widgetText.centerAlignText()
+return testWidget
+}
