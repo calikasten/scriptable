6e823365f6a4b90a6a52c7fb77de7c75dcaeee0e
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2023-05-03 12:44:54 -0500

    Update Static Text Widget.js

--- Static Text Widget.js
+++ Static Text Widget.js
@@ -1,7 +1,6 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: font;
-
 let widget = await createWidget()
 
 // Check where the script is running
@@ -16,12 +15,15 @@ Script.complete();
 // Create widget
 async function createWidget() {
 	let testWidget = new ListWidget()
-  // Add widget background
+  		// Add widget background
 		testWidget.backgroundColor = new Color("#000000");
 	// Add text to a widget
 	let widgetText = testWidget.addText("INSERT TEXT HERE");
+		// Set widget text font and size
 		widgetText.font = Font.boldSystemFont(15)
+		// Set widget text color
 		widgetText.textColor = new Color ("#ffffff")
+		// Set widget text alignment
 		widgetText.centerAlignText()
 // Display widget
 return testWidget
