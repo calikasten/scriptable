f47490a21fc2efb4a684b0508a48bd944f122032
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2023-05-30 12:21:57 -0500

    Update Static Text Widget.js

--- Static Text Widget.js
+++ Static Text Widget.js
@@ -1,42 +1,41 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: font;
-const widget = await createWidget()
+const widget = await createWidget();
 
 // Check where the script is running
 if (config.runsInWidget) {
-  Script.setWidget(widget)
-} 
-else {
-  widget.presentSmall()
+  Script.setWidget(widget);
+} else {
+  widget.presentSmall();
 }
 Script.complete();
 
 // Create widget
 async function createWidget() {
-const testWidget = new ListWidget()
-
-// Add single, solid color background to widget
-// testWidget.backgroundColor = new Color("#000000")
-
-// Add gradient background to widget
-const gradient = new LinearGradient()
-	gradient.locations = [0 1]
-	gradient.colors = [new Color("000000"), new Color("#1C1B1D")]
-	testWidget.backgroundGradient = gradient
-
-// Add text to a widget
-const widgetText = testWidget.addText("INSERT TEXT HERE")
-
-	// Set widget text font and size
-	widgetText.font = Font.boldSystemFont(15)
+  const testWidget = new ListWidget();
 	
-	// Set widget text color
-	widgetText.textColor = new Color ("#ffffff")
-		
-	// Set widget text alignment
-	widgetText.centerAlignText()
+	// Add single, solid color background to widget
+  // testWidget.backgroundColor = new Color("#000000")
+
+  // Add gradient background to widget
+  const gradient = new LinearGradient();
+  gradient.locations = [0, 1];
+  gradient.colors = [new Color("#000000"), new Color("#1C1B1D")];
+  testWidget.backgroundGradient = gradient;
 	
-// Display widget
-return testWidget
-};
+	// Add text to a widget
+  const widgetText = testWidget.addText("INSERT TEXT HERE");
+	
+  // Set widget text font and size
+  widgetText.font = Font.boldSystemFont(15);
+	
+  // Set widget text color
+  widgetText.textColor = new Color("#ffffff");
+	
+  // Set widget text alignment
+  widgetText.centerAlignText();
+	
+  // Display widget
+  return testWidget;
+}
