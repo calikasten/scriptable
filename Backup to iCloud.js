40fe19a47829fc184ed9304aebe918d9c385b7ed
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2024-06-22 10:12:46 -0500

    Update Backup to iCloud.js

--- Backup to iCloud.js
+++ Backup to iCloud.js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: light-gray; icon-glyph: cloud;
+
 iCloud = FileManager.iCloud();
 directory = iCloud.documentsDirectory();
 
