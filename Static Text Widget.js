ab8dc50026254950b9c55c470b51ba6b0d0938bc
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2024-06-22 09:51:21 -0500

    Update Static Text Widget.js

--- Static Text Widget.js
+++ Static Text Widget.js
@@ -1,41 +1,43 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-gray; icon-glyph: font;
-const widget = await createWidget();
+
+let widget = await createWidget();
 
 // Check where the script is running
 if (config.runsInWidget) {
-  Script.setWidget(widget);
-} else {
-  widget.presentSmall();
+	Script.setWidget(widget);
+} 
+else {
+	widget.presentSmall();
 }
 Script.complete();
 
 // Create widget
 async function createWidget() {
-  const testWidget = new ListWidget();
+	const newWidget = new ListWidget();
 	
 	// Add single, solid color background to widget
-  // testWidget.backgroundColor = new Color("#000000")
-
-  // Add gradient background to widget
-  const gradient = new LinearGradient();
-  gradient.locations = [0, 1];
-  gradient.colors = [new Color("#000000"), new Color("#1C1B1D")];
-  testWidget.backgroundGradient = gradient;
+	// testWidget.backgroundColor = new Color("#000000");
+	
+	// Add gradient background to widget
+	const gradient = new LinearGradient();
+	gradient.locations = [0, 1];
+	gradient.colors = [new Color("#000000"), new Color("#1C1B1D")];
+	newWidget.backgroundGradient = gradient;
 	
 	// Add text to a widget
-  const widgetText = testWidget.addText("INSERT TEXT HERE");
+	const widgetText = newWidget.addText("INSERT TEXT HERE");
 	
-  // Set widget text font and size
-  widgetText.font = Font.boldSystemFont(15);
+	// Set widget text font and size 
+	widgetText.font = Font.boldSystemFont(15);
 	
-  // Set widget text color
-  widgetText.textColor = new Color("#ffffff");
+	// Set widget text color
+	widgetText.textColor = new Color("#ffffff");
 	
-  // Set widget text alignment
-  widgetText.centerAlignText();
+	// Set widget text alignment
+	widgetText.centerAlignText();
 	
-  // Display widget
-  return testWidget;
+// Display widget
+return newWidget;
 }
