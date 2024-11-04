2998f3630456c198cbec6ff3f6b10872e2528f0b
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2024-11-04 10:05:50 -0600

    Update JSON Viewer.js

--- JSON Viewer.js
+++ JSON Viewer.js
@@ -12,28 +12,28 @@ if (url != null) {
   	let request = new Request(url);
   	let json = await request.loadJSON();
   	await input(json);
-} 
+}; 
 else if (fileURL != null) {
   
   	// Load the JSON from a file URL input and parse it into an object
 	let fileManager = FileManager.local();
 	let text = fileManager.readString(fileURL);
 	 await parseAndPrompt(text);
-} 
+}; 
 else if (text != null) {
 	// Load plain text input and parse it into an object
  	 await parseAndPrompt(text);
-} 
+}; 
 else {
  	// Error for invalid input
 	let alert = new Alert();
 	alert.title = "No valid input provided.";
 	alert.addCancelAction("OK");
 	await alert.present();
-}
+};
 async function input(json) {
 	await prettyPrint(json);
-} 
+}; 
 async function prettyPrint(json) {
 	let str = JSON.stringify(json, null, 2);
  	await QuickLook.present(str);
