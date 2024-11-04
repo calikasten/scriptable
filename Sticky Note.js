8ce95a603dc44072d4981ea55d6c44c94c743cb1
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2024-11-04 12:25:23 -0600

    Update Sticky Note.js

--- Sticky Note.js
+++ Sticky Note.js
@@ -11,33 +11,33 @@ const directoryPath = `${directory}${folderLocation}`;
 
 // Function to load saved sticky note text
 function loadData() {
-  try {
-    let filePath = iCloud.joinPath(directoryPath, "sticky-note.txt")
-    return iCloud.readString(filePath)
-  } catch(error) {
-    return data
-  }
+	try {
+	let filePath = iCloud.joinPath(directoryPath, "sticky-note.txt")
+    	return iCloud.readString(filePath)
+  	} catch(error) {
+    		return data
+  	}
 }
 
 // Function to enter new text for sticky note
 async function editData(data) {
-  let editor = new Alert()
-  editor.title = "Enter Sticky Note Text"
-  editor.addTextField(data)
-  editor.addCancelAction("Cancel")
-  editor.addAction("Save")
-  let action = await editor.present()
-  if (action < 0) {
-    return data
-  } else {
-    return editor.textFieldValue(0)
-  }
+	let editor = new Alert()
+  	editor.title = "Enter Sticky Note Text"
+  	editor.addTextField(data)
+  	editor.addCancelAction("Cancel")
+  	editor.addAction("Save")
+  	let action = await editor.present()
+  	if (action < 0) {
+    		return data
+  	} else {
+    		return editor.textFieldValue(0)
+  	}
 }
 
 // Function to create new directory location and save entered sticky note text
 function saveData(data) {
 	let filePath = iCloud.joinPath(directoryPath, "sticky-note.txt");
-  iCloud.writeString(filePath, data);
+  	iCloud.writeString(filePath, data);
 }
 
 // Retreive and modify background image for widget
@@ -64,13 +64,13 @@ function saveData(data) {
 	if (imageAspectRatio > widgetAspectRatio) {
   		
       // If image is wider than the widget, fit height and zoom in slightly
-    drawHeight = widgetHeight * zoomFactor;
-    drawWidth = drawHeight * imageAspectRatio;
+    	drawHeight = widgetHeight * zoomFactor;
+    	drawWidth = drawHeight * imageAspectRatio;
 	} else {
     
-    // If image is taller than the widget, fit width and zoom in slightly
-    drawWidth = widgetWidth * zoomFactor;
-    drawHeight = drawWidth / imageAspectRatio;
+    		// If image is taller than the widget, fit width and zoom in slightly
+    		drawWidth = widgetWidth * zoomFactor;
+    		drawHeight = drawWidth / imageAspectRatio;
 	}
 
 	// Center the zoomed image within the widget dimensions
@@ -80,34 +80,33 @@ function saveData(data) {
 
 // Function to create and customize widget UI
 function createWidget(note) {
-  let widget = new ListWidget()
+	let widget = new ListWidget()
 
-  // Set zoomed image as background for widget
+  	// Set zoomed image as background for widget
 	let zoomedImage = context.getImage();
 	widget.backgroundImage = zoomedImage
   
 	// Add entered sticky note text to widget
-  widget.addSpacer(20);
-  let noteText = widget.addText(note)
-  noteText.textColor = Color.black()
-  noteText.font = Font.mediumRoundedSystemFont(16)
-  noteText.centerAlignText();
+  	widget.addSpacer(20);
+  	let noteText = widget.addText(note)
+  	noteText.textColor = Color.black()
+  	noteText.font = Font.mediumRoundedSystemFont(16)
+  	noteText.centerAlignText();
 	widget.addSpacer();
     
-  // Return customized widget UI
-  return widget;
+  	// Return customized widget UI
+  	return widget;
 }
 
 // Check where script is running, ask for input and display preview
 let note = loadData()
 if (config.runsInWidget) {
 	let widget = createWidget(note)
-  Script.setWidget(widget)
+  	Script.setWidget(widget)
 } else {
-  note = await editData(note)
-  saveData(note)
-  let widget = createWidget(note)
-  widget.presentLarge()
+	note = await editData(note)
+  	saveData(note)
+  	let widget = createWidget(note)
+  	widget.presentLarge()
 };
-
 Script.complete();
