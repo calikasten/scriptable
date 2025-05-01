2c1d5450f2718b3f4e6cdd12da818dcd55d51b82
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2025-05-01 15:55:23 -0500

    Update Dog Days 2.0.js

--- Dog Days 2.0.js
+++ Dog Days 2.0.js
@@ -3,196 +3,107 @@
 // icon-color: pink; icon-glyph: paw;
 
 // Set API variables
-const API_TOKEN = "<INSERT API KEY HERE>";
-const APP_ID = "<INSERT APP ID HERE>";
-const TABLE_ID = "<INSERT TABLE ID HERE>";
+const API_TOKEN = "INSERT API TOKEN HERE";
+const APP_ID = "INSERT APP ID HERE";
+const TABLE_ID = "INSERT TABLE ID HERE";
 
-// Begin functions to get data from API
+const TITLE_TEXT = "Barley 🐶";
 
-// Lastest pee timestamp
-async function timeSinceLastPee() {
-  
-  // Start API call
-  const url = "https://api.airtable.com/v0/" + APP_ID + "/" + TABLE_ID + "/?maxRecords=1&view=Pee";
-  const newRequest = new Request(url);
-  newRequest.headers = { Authorization: "Bearer " + API_TOKEN };
-  const json = await newRequest.loadJSON();
-  
-  // Isolate fields and values from API response
-  const records = json["records"];
-  const fields = records["0"]["fields"];
-  const timestamp = fields["Timestamp"];
-        
-  // Get time difference between current time and response timestamp
-  let timestampRaw = new Date(timestamp);
-  let now = new Date();
-  let timeDiffMS = now - timestampRaw;
-  let timeDiff = (timeDiffMS / (1000 * 60 * 60)).toFixed(2);
-  
-  // Function returns the time
-  return timeDiff;
-};
+// List of activities as Airtable views to surface as list elements
+const views = [
+  { name: "Pee", emoji: "💛" },
+  { name: "Poop", emoji: "💩" },
+  { name: "Walk", emoji: "🚶" },
+  { name: "Food", emoji: "🍗" },
+];
 
-// Lastest  poop timestamp
-async function timeSinceLastPoop() {
-  
-  // Start API call
-  const url = "https://api.airtable.com/v0/" + APP_ID + "/" + TABLE_ID + "/?maxRecords=1&view=Poop";
-  const newRequest = new Request(url);
-  newRequest.headers = { Authorization: "Bearer " + API_TOKEN };
-  const json = await newRequest.loadJSON();
-        
-  // Isolate fields and values from API response
-  const records = json["records"];
-  const fields = records["0"]["fields"];
-  const timestamp = fields["Timestamp"];
-        
-  // Get time difference between current time and response timestamp
-  let timestampRaw = new Date(timestamp);
-  let now = new Date();
-  let timeDiffMS = now - timestampRaw;
-  let timeDiff = (timeDiffMS / (1000 * 60 * 60)).toFixed(2);
-        
-  // Function returns the time
-  return timeDiff;
-};
+// Function to get data from API
+async function fetchLatestTimestamp(viewName) {
+  const url = `https://api.airtable.com/v0/${APP_ID}/${TABLE_ID}?maxRecords=1&view=${encodeURIComponent(
+    viewName
+  )}&sort[0][field]=Timestamp&sort[0][direction]=desc`;
+  try {
+    const request = new Request(url);
+    request.headers = {
+      Authorization: `Bearer ${API_TOKEN}`,
+    };
+    const response = await request.loadJSON(); 
+    
+    // Check if the response is valid, log response, and return data
+    if (response.records.length === 0) return null;
+    console.log(response.records[0].fields["Timestamp"]);
+    return response.records[0].fields["Timestamp"];
+  } catch (error) {
+    console.error(`Error fetching data from API for ${viewName}.`);
+    return null;
+  }
+}
 
-// Latest food timestamp
-async function timeSinceLastFood() {
-  
-  // Start API call
-  const url = "https://api.airtable.com/v0/" + APP_ID + "/" + TABLE_ID + "/?maxRecords=1&view=Food";
-  const newRequest = new Request(url);
-  newRequest.headers = { Authorization: "Bearer " + API_TOKEN };
-  const json = await newRequest.loadJSON();
-        
-  // Isolate fields and values from API response
-  const records = json["records"];
-  const fields = records["0"]["fields"];
-  const timestamp = fields["Timestamp"];
-        
-  // Get time difference between current time and response timestamp
-  let timestampRaw = new Date(timestamp);
-  let now = new Date();
-  let timeDiffMS = now - timestampRaw;
-  let timeDiff = (timeDiffMS / (1000 * 60 * 60)).toFixed(2);
-        
-  // Function returns the time
-  return timeDiff;
-};
+// Function to calculate time difference
+function timeDiff(timestampStr) {
+  if (!timestampStr) return "N/A";
+  const date = new Date(timestampStr);
+  const diffMS = new Date() - date;
+  return (diffMS / (1000 * 60 * 60)).toFixed(1);
+}
 
-// Lastest water timestamp
-async function timeSinceLastWater() {
+// Helper function to apply consistent text styling to list elements
+function applyTextStyle(widget, emoji, timeDiff) {
+  const listElements = widget.addStack();
+  listElements.centerAlignContent();
+  listElements.layoutHorizontally();
+  listElements.setPadding(5, 6, 0, 0); 
   
-  // Start API call
-  const url = "https://api.airtable.com/v0/" + APP_ID + "/" + TABLE_ID + "/?maxRecords=1&view=Water";
-  const newRequest = new Request(url);
-  newRequest.headers = { Authorization: "Bearer " + API_TOKEN };
-  const json = await newRequest.loadJSON();
+  // Style emoji
+  const emojiText = listElements.addText(`${emoji} `);
+  emojiText.font = Font.semiboldSystemFont(14); 
   
-  // Isolate fields and values from API response
-  const records = json["records"];
-  const fields = records["0"]["fields"];
-  const timestamp = fields["Timestamp"];
+  // Style time difference text
+  const timeDiffText = listElements.addText(`${timeDiff} `);
+  timeDiffText.textColor = Color.white();
+  timeDiffText.font = Font.semiboldSystemFont(14); 
   
-  // Get time difference between current time and response timestamp
-  let timestampRaw = new Date(timestamp);
-  let now = new Date();
-  let timeDiffMS = now - timestampRaw;
-  let timeDiff = (timeDiffMS / (1000 * 60 * 60)).toFixed(2);
-  
-  // Function returns the time
-  return timeDiff;
-};
-
-// Latest walk timestamp
-async function timeSinceLastWalk() {
-  
-  // Start API call
-  const url = "https://api.airtable.com/v0/" + APP_ID + "/" + TABLE_ID + "/?maxRecords=1&view=Exercise";
-  const newRequest = new Request(url);
-  newRequest.headers = { Authorization: "Bearer " + API_TOKEN };
-  const json = await newRequest.loadJSON();
-
-  // Isolate fields and values from API response
-  const records = json["records"];
-  const fields = records["0"]["fields"];
-  const timestamp = fields["Timestamp"];
-  
-  // Get time difference between current time and response timestamp
-  let timestampRaw = new Date(timestamp);
-  let now = new Date();
-  let timeDiffMS = now - timestampRaw;
-  let timeDiff = (timeDiffMS / (1000 * 60 * 60)).toFixed(2);
-  
-  // Function returns the time
-  return timeDiff;
-};
+  // Style "hours ago" suffix text
+  const suffixStack = listElements.addStack();
+  suffixStack.layoutVertically();
+  suffixStack.addSpacer(4);
+  const suffixText = suffixStack.addText("hours ago");
+  suffixText.textColor = Color.gray();
+  suffixText.font = Font.systemFont(12);
+  return listElements;
+}
 
 // Function to create and customize widget UI
 async function createWidget() {
-  const dogDays = new ListWidget();
+  const widget = new ListWidget();
+  widget.backgroundColor = new Color("#1C1B1D"); 
   
-  // Add widget background
-  dogDays.backgroundColor = new Color("#1C1B1D");
+  // Add widget title
+  const title = widget.addText(TITLE_TEXT);
+  title.centerAlignText();
+  title.font = Font.semiboldSystemFont(20);
+  title.textColor = new Color("#ffffff");
+  widget.addSpacer(); 
   
-  // Add widget heading
-  const heading = dogDays.addText("Barley 🐶");
-  heading.centerAlignText();
-  heading.font = Font.semiboldSystemFont(15);
-  heading.textColor = new Color("#ffffff");
-  dogDays.addSpacer();
-  
-  // Add time since last pee
-  const itemPee = dogDays.addStack();
-  const lastPee = await timeSinceLastPee();
-  let line1 = itemPee.addText("💛 " + lastPee + " hours ago");
-  line1.font = Font.semiboldSystemFont(12);
-  line1.leftAlignText();
-  
-  // Add time since last poop
-  const itemPoop = dogDays.addStack();
-  const lastPoop = await timeSinceLastPoop();
-  let line2 = itemPoop.addText("💩 " + lastPoop + " hours ago");
-  line2.font = Font.semiboldSystemFont(12);
-  line2.leftAlignText();
-  
-  // Add time since last food
-  const itemFood = dogDays.addStack();
-  const lastFood = await timeSinceLastFood();
-  let line3 = itemFood.addText("🍗 " + lastFood + " hours ago");
-  line3.font = Font.semiboldSystemFont(12);
-  line3.leftAlignText();
-  
-  // Add time since last water
-  const itemWater = dogDays.addStack();
-  const lastWater = await timeSinceLastWater();
-  let line4 = itemWater.addText("💧 " + lastWater + " hours ago");
-  line4.font = Font.semiboldSystemFont(12);
-  line4.leftAlignText();
-  
-  // Add time since last walk
-  const itemWalk = dogDays.addStack();
-  const lastWalk = await timeSinceLastWalk();
-  let line5 = itemWalk.addText("🚶‍♀️ " + lastWalk + " hours ago");
-  line5.font = Font.semiboldSystemFont(12);
-  line5.leftAlignText();
-  dogDays.addSpacer();
-  
-  // Display widget
-  return dogDays;
-};
+  // Loop through views for latest timestamp and add as list element to widget
+  for (let view of views) {
+    const timestamp = await fetchLatestTimestamp(view.name);
+    applyTextStyle(widget, view.emoji, timeDiff(timestamp));
+  }
+  widget.addSpacer(); 
+
+  // Return customized widget UI
+  return widget;
+}
 
 // Display widget
 let widget = await createWidget();
 
 // Check where the script is running
 if (config.runsInWidget) {
-        
-  // Run inside a widget
+  // Run inside widget
   Script.setWidget(widget);
 } else {
-
   // Otherwise show preview
   widget.presentSmall();
 }
