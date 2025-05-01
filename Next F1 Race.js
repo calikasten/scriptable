30b0e61e48d141d3ea00af13de158e0d5d062a77
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2025-05-01 09:30:05 -0500

    Update Next F1 Race.js

--- Next F1 Race.js
+++ Next F1 Race.js
@@ -49,20 +49,30 @@ function aliasCountryName(countryName) {
 // Fetch data from API and parse response
 const data = await getData();
 const raceName = data.race.meetingOfficialName;
-const raceCountryName = aliasCountryName(data.race.meetingCountryName).toLowerCase().replace(/\s/g, "-");
-const flagUrl = "https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Flags%2016x9/" + raceCountryName + "-flag.png";
+const raceCountryName = aliasCountryName(data.race.meetingCountryName)
+  .toLowerCase()
+  .replace(/\s/g, "-");
+const flagUrl =
+  "https://www.formula1.com/content/dam/fom-website/2018-redesign-assets/Flags%2016x9/" +
+  raceCountryName +
+  "-flag.png";
 const circuitUrl = data.circuitSmallImage.url.slice(0, -4) + "%20carbon.png";
 
 // Function to load all images (logo, flag, circuit)
 const [logo, flag, circuit] = await Promise.all([
-  getImage("https://calikasten.wordpress.com/wp-content/uploads/2025/04/f1-logo.png"),
+  getImage(
+    "https://calikasten.wordpress.com/wp-content/uploads/2025/04/f1-logo.png"
+  ),
   getImage(flagUrl),
   getImage(circuitUrl),
 ]);
 
 // Function to get next race event
+const events = data.seasonContext.timetables;
+const event = getNextEvent(events);
+const eventTime = event.startTime + event.gmtOffset;
+
 function getNextEvent(events) {
-  
   // Check if the response is valid
   if (!events || events.length === 0) {
     return {
@@ -76,8 +86,10 @@ function getNextEvent(events) {
   events.sort((a, b) => (a.startTime < b.startTime ? -1 : 1));
   const now = new Date();
   for (let event of events) {
-    const eventDate = new Date(Date.parse(event.startTime) + event.gmtOffset * 1000);
-    if (eventDate > now) return event
+    const eventDate = new Date(
+      Date.parse(event.startTime) + event.gmtOffset * 1000
+    );
+    if (eventDate > now) return event;
     return event;
   }
 
@@ -88,15 +100,31 @@ function getNextEvent(events) {
   };
 }
 
+// Format next event race date/time
+const dateObj = new Date(eventTime);
+const formatDay = {
+  weekday: "long",
+};
+const formatTime = {
+  hour: "numeric",
+  minute: "numeric",
+  hour12: false,
+};
+
+const weekday = Intl.DateTimeFormat("en", formatDay).format(dateObj);
+const timeStr = Intl.DateTimeFormat("en", formatTime).format(dateObj);
+const dateStr = weekday + ", " + timeStr;
+if (!eventTime) dateStr = "";
+
 // Function to calculate countdown until next race event
 function getCountdown(dateObj) {
   if (!dateObj) {
     return ["00", "00", "00"];
   }
-  
+
   // Calculate time difference
-  const diff = (Date.parse(dateObj) - Date.parse(new Date())) / 1000; 
-  
+  const diff = (Date.parse(dateObj) - Date.parse(new Date())) / 1000;
+
   // Set difference in days, hours, and minutes
   let day = Math.floor(diff / 60 / 60 / 24);
   let hour = Math.floor((diff / 60 / 60) % 24);
@@ -122,85 +150,70 @@ function createWidget(data) {
   const widget = new ListWidget();
   widget.setPadding(0, 0, 0, 0);
   widget.backgroundColor = Color.white();
-  
-  const accentColor = new Color("E10600"); 
-  
-  // Add header banner with race country flag, official race name, and F1 logo
+
+  const accentColor = new Color("E10600");
+
+  // Add header banner
   const header = widget.addStack();
   header.backgroundColor = accentColor;
   header.setPadding(20, 20, 10, 20);
-  
+
+  // Add race country flag
   const raceCountryFlag = header.addImage(flag);
   raceCountryFlag.imageSize = new Size(50, 30);
   raceCountryFlag.cornerRadius = 3;
   raceCountryFlag.borderColor = Color.white();
   raceCountryFlag.borderWidth = 2;
-  
+
   header.addSpacer();
-  
+
+  // Add official race name
   const officialRaceName = header.addText(raceName);
   officialRaceName.textColor = Color.white();
   officialRaceName.font = Font.boldSystemFont(13);
   officialRaceName.centerAlignText();
-  
+
   header.addSpacer();
-  
+
+  // Add F1 logo
   const F1logo = header.addImage(logo);
-  F1logo.imageSize = new Size(60, 30); 
-  
+  F1logo.imageSize = new Size(60, 30);
+
   // Add next race event details
   const leftColumn = widget.addStack();
   leftColumn.setPadding(5, 35, 5, 35);
-  
+
   const leftColumnTopRow = leftColumn.addStack();
   leftColumnTopRow.layoutVertically();
   leftColumnTopRow.addSpacer();
-  
-  const events = data.seasonContext.timetables;
-  const event = getNextEvent(events);
-  const eventTime = event.startTime + event.gmtOffset;
-  
+
   const nextEventName = leftColumnTopRow.addText(event.description);
   nextEventName.textColor = accentColor;
-  nextEventName.font = Font.boldSystemFont(20); 
-  
+  nextEventName.font = Font.boldSystemFont(20);
+
   // Add countdown until next race event
   const leftColumnBottomRow = leftColumnTopRow.addStack();
   leftColumnBottomRow.bottomAlignContent();
-  
+
   const [day, hour, minute] = getCountdown(eventTime);
   formatCountdownText(leftColumnBottomRow, day + ":");
   formatCountdownText(leftColumnBottomRow, hour + ":");
   formatCountdownText(leftColumnBottomRow, minute);
-  
-  leftColumnTopRow.addSpacer(0); 
-  
-  // Add next race event date and time
-  const dateObj = new Date(eventTime);
-  const formatDay = {
-    weekday: "long",
-  };
-  const formatTime = {
-    hour: "numeric",
-    minute: "numeric",
-    hour12: false,
-  };
 
-  const weekday = Intl.DateTimeFormat("en", formatDay).format(dateObj);
-  const timeStr = Intl.DateTimeFormat("en", formatTime).format(dateObj);
-  const dateStr = weekday + ", " + timeStr;
-  if (!eventTime) dateStr = "";
+  leftColumnTopRow.addSpacer(0);
+
+  // Add next race event date and time
   const nextEventTime = leftColumnTopRow.addText(dateStr);
   nextEventTime.textColor = Color.black();
   nextEventTime.font = Font.regularSystemFont(12);
-  
+
   leftColumn.addSpacer();
-  leftColumnTopRow.addSpacer(); 
-  
+  leftColumnTopRow.addSpacer();
+
   // Add image of race track circuit
   const raceCircuit = leftColumn.addImage(circuit);
-  raceCircuit.imageSize = new Size(130, 108); 
-  
+  raceCircuit.imageSize = new Size(130, 108);
+
   // Return customized widget UI
   return widget;
 }
@@ -210,11 +223,9 @@ const widget = await createWidget(data);
 
 // Check where the script is running
 if (config.runsInWidget) {
-  
   // Run inside widget
   Script.setWidget(widget);
 } else {
-  
   // Otherwise show preview
   widget.presentMedium();
 }
