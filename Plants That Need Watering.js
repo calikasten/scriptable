dcc7e2bb584d96fe1796c2c6806ee36c64345f60
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2025-10-21 08:57:13 -0500

    Update Plants That Need Watering.js

--- Plants That Need Watering.js
+++ Plants That Need Watering.js
@@ -1,6 +1,7 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-green; icon-glyph: leaf;
+
 // === CONFIGURATION ===
 const CONFIG = {
   folder: "Plant Data",
