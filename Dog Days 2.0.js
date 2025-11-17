94ccc00cd989a3e72356e50ea1f206e861d6a073
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2025-11-16 21:21:02 -0600

    Refactor Dog Days script with updated API details

--- Dog Days 2.0.js
+++ Dog Days 2.0.js
@@ -1,13 +1,11 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
-// icon-color: pink; icon-glyph: paw;
+// icon-color: teal; icon-glyph: paw;
 
-// Set API variables
+// === CONFIGURATION ==
 const API_TOKEN = "<INSERT API TOKEN HERE>";
-const APP_ID = "<INSERT APP ID HERE>";
-const TABLE_ID = "<INSERT TABLE ID HERE>";
-
-const TITLE_TEXT = "Barley 🐶";
+const APP_ID = "app1PnYsdLk3i0N6S";
+const TABLE_ID = "tbl4cXnZhwuW1TBj4";
 
 // List of activities as Airtable views to surface as list elements
 const views = [
@@ -17,29 +15,21 @@ const views = [
   { name: "Food", emoji: "🍗" },
 ];
 
-// Function to get data from API
-async function fetchLatestTimestamp(viewName) {
-  const url = `https://api.airtable.com/v0/${APP_ID}/${TABLE_ID}?maxRecords=1&view=${encodeURIComponent(
-    viewName
-  )}&sort[0][field]=Timestamp&sort[0][direction]=desc`;
-  try {
-    const request = new Request(url);
-    request.headers = {
-      Authorization: `Bearer ${API_TOKEN}`,
-    };
-    const response = await request.loadJSON(); 
-    
-    // Check if the response is valid, log response, and return data
-    if (response.records.length === 0) return null;
-    console.log(response.records[0].fields["Timestamp"]);
-    return response.records[0].fields["Timestamp"];
-  } catch (error) {
-    console.error(`Error fetching data from API for ${viewName}.`);
-    return null;
-  }
-}
+// === STYLES ===
+// Format font sizes
+const TITLE_FONT = Font.semiboldSystemFont(20);
+const LIST_TEXT_FONT = Font.semiboldSystemFont(14);
+const LIST_SUFFIX_FONT = Font.systemFont(12);
 
-// Function to calculate time difference
+// Format font colors
+const TEXT_COLOR = Color.white();
+const SUFFIX_TEXT_COLOR = Color.white();
+
+// Format background color
+const WIDGET_BACKGROUND_COLOR = new Color("1C1B1D");
+
+// === HELPERS ===
+// Calculate time difference
 function timeDiff(timestampStr) {
   if (!timestampStr) return "N/A";
   const date = new Date(timestampStr);
@@ -47,7 +37,7 @@ function timeDiff(timestampStr) {
   return (diffMS / (1000 * 60 * 60)).toFixed(1);
 }
 
-// Helper function to apply consistent text styling to list elements
+// Apply consistent styling to list elements
 function applyTextStyle(widget, emoji, timeDiff) {
   const listElements = widget.addStack();
   listElements.centerAlignContent();
@@ -73,38 +63,59 @@ function applyTextStyle(widget, emoji, timeDiff) {
   return listElements;
 }
 
-// Function to create and customize widget UI
+// === API CLIENT ===
+// Function to get data from API
+async function fetchLatestTimestamp(viewName) {
+  const url = `https://api.airtable.com/v0/${APP_ID}/${TABLE_ID}?maxRecords=1&view=${encodeURIComponent(
+    viewName
+  )}&sort[0][field]=Timestamp&sort[0][direction]=desc`;
+  try {
+    const request = new Request(url);
+    request.headers = {
+      Authorization: `Bearer ${API_TOKEN}`,
+    };
+    const response = await request.loadJSON(); 
+    
+    // Check if the response is valid, log response, and return data
+    if (response.records.length === 0) return null;
+    console.log(response.records[0].fields["Timestamp"]);
+    return response.records[0].fields["Timestamp"];
+  } catch (error) {
+    console.error(`Error fetching data from API for ${viewName}.`);
+    return null;
+  }
+}
+
+// === UI COMPONENTS ===
 async function createWidget() {
   const widget = new ListWidget();
   widget.backgroundColor = new Color("#1C1B1D"); 
   
-  // Add widget title
-  const title = widget.addText(TITLE_TEXT);
+  // Widget title
+  const title = widget.addText("Barley 🐶");
   title.centerAlignText();
   title.font = Font.semiboldSystemFont(20);
   title.textColor = new Color("#ffffff");
   widget.addSpacer(); 
   
-  // Loop through views for latest timestamp and add as list element to widget
+  // Add a list entry for each activity with its most recent timestamp
   for (let view of views) {
     const timestamp = await fetchLatestTimestamp(view.name);
     applyTextStyle(widget, view.emoji, timeDiff(timestamp));
   }
   widget.addSpacer(); 
-
-  // Return customized widget UI
+  
+  // Return widget with its constructed UI elements
   return widget;
 }
 
-// Display widget
+// === WIDGET ASSEMBLY ===
 let widget = await createWidget();
 
-// Check where the script is running
+// === MAIN EXECUTION ===
 if (config.runsInWidget) {
-  // Run inside widget
   Script.setWidget(widget);
 } else {
-  // Otherwise show preview
   widget.presentSmall();
 }
 Script.complete();
