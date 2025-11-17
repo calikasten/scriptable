b1b95e829482bc0560e93164552f5caa8a7ac639
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2025-11-16 21:42:20 -0600

    Update Widget Template (Local).js

--- Widget Template (Local).js
+++ Widget Template (Local).js
@@ -2,16 +2,13 @@
 // Format date in MM-dd-yyyy
 const DATE_FORMAT = "MM-dd-yyyy";
 
-// Format font style and size
-const TITLE_FONT_SIZE = 16;
-const BODY_FONT_SIZE = 10;
-const TITLE_FONT = Font.boldSystemFont(TITLE_FONT_SIZE);
-const FONT = Font.semiboldSystemFont(BODY_FONT_SIZE);
-
-// Format text colors
-const TEXT_COLOR = new Color("#FFFF00");
-const TITLE_COLOR = new Color("#FFFFFF");
-const ERROR_COLOR = new Color("#FF3B30");
+// Format font size and color
+const STYLES = {
+	TITLE_FONT: Font.boldSystemFont(16),
+	TITLE_COLOR: new Color("#FFFFFF"),
+	TEXT_FONT: Font.semiboldSystemFont(10),
+	TEXT_COLOR: new Color("FFFF00")
+};
 
 // === UI COMPONENTS ===
 function createWidget() {
@@ -19,8 +16,8 @@ function createWidget() {
 
   // Widget title
   const title = widget.addText("TITLE");
-  title.font = TITLE_FONT;
-  title.textColor = TITLE_COLOR;
+  title.font = STYLES.TITLE_FONT;
+  title.textColor = STYLES.TITLE_COLOR;
   title.centerAlignText();
   widget.addSpacer(5);
 
@@ -30,8 +27,8 @@ function createWidget() {
   // Number each line of widget data
   const lines = widgetData.map((value, i) => `${i + 1}. ${value}`);
   const textBlock = widget.addText(lines.join("\n"));
-  textBlock.font = FONT;
-  textBlock.textColor = TEXT_COLOR;
+  textBlock.font = STYLES.TEXT_FONT;
+  textBlock.textColor = STYLES.TEXT_COLOR;
   textBlock.leftAlignText();
 
   // Return widget with its constructed UI elements
