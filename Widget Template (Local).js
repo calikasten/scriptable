016dfc173584ca09773a6051e84e4859454e87bf
Cali Kasten <91810027+calikasten@users.noreply.github.com>
2025-11-21 11:55:56 -0800

    Update Widget Template (Local).js

--- Widget Template (Local).js
+++ Widget Template (Local).js
@@ -1,3 +1,7 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: deep-gray; icon-glyph: window-maximize;
+
 // === CONFIGURATION ===
 // This section stores all "settings", nothing here should change during execution
 
